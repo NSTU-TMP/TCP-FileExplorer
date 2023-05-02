d4398e2ba382cd52680d64e249755811f95328a6
danilka108 <danilka108@yandex.ru>
2023-05-02 14:51:05 +0700

    add handler errors catching

--- server/lserver/server.cpp
+++ server/lserver/server.cpp
@@ -2,10 +2,10 @@
 
 #include "handler.hpp"
 #include "ip_addr.hpp"
+#include <exception>
 
 server::server(ip_addr address, uint16_t port, size_t _max_connections_count)
-    : listener{address, port},
-      max_connections_count(_max_connections_count),
+    : listener{address, port}, max_connections_count(_max_connections_count),
       ip(address) {}
 
 void server::check_threads() {
@@ -25,9 +25,13 @@ void server::listen_clients() {
     if (threads.size() < this->max_connections_count) {
       tcp_client client = this->listener.accept_client();
       ip_addr server_ip = this->ip;
+
       std::thread thr([client = std::move(client), &server_ip]() mutable {
-        handler client_handler(std::move(client), server_ip);
-        client_handler.handle();
+        try {
+          handler client_handler(std::move(client), server_ip);
+          client_handler.handle();
+        } catch (std::exception &ex) {
+        }
       });
 
       threads.push_back(std::move(thr));
